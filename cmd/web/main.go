3f95eff05112f8e3bff0f032bd57cdae3ea57eb9
DevJ1003 <devjoshi1384@gmail.com>
2023-09-29 01:09:18 +0530

    commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/devj1003/mygoapp/pkg/config"
 	"github.com/devj1003/mygoapp/pkg/handlers"
+	"github.com/devj1003/mygoapp/pkg/render"
 )
 
 // Port number function
@@ -13,8 +16,24 @@ const portNumber = ":8081"
 // Main func
 func main() {
 
-	http.HandleFunc("/", handlers.HomePage)
-	http.HandleFunc("/about", handlers.AboutPage)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		// fmt.Println(err)
+		log.Fatal("Cannot create template!")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.HomePage)
+	http.HandleFunc("/about", handlers.Repo.AboutPage)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 	http.ListenAndServe(portNumber, nil)
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -3,15 +3,46 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/devj1003/mygoapp/pkg/config"
+	"github.com/devj1003/mygoapp/pkg/models"
 	"github.com/devj1003/mygoapp/pkg/render"
 )
 
+// Repo the repository used for the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates a new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
 // HomePage func for home page handler
-func HomePage(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "home.page.html")
+func (m *Repository) HomePage(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
 // AboutPage func for about page handler
-func AboutPage(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.page.html")
+func (m *Repository) AboutPage(w http.ResponseWriter, r *http.Request) {
+
+	// perform some logic
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello, again!"
+
+	// send the data to the template
+	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
+		StringMap: stringMap,
+	})
+
 }
