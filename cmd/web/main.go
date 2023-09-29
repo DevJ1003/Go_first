ce49e851f860d7e9b0868a82f3f196adf701edea
DevJ1003 <devjoshi1384@gmail.com>
2023-09-30 01:21:57 +0530

    commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/devj1003/mygoapp/pkg/config"
 	"github.com/devj1003/mygoapp/pkg/handlers"
 	"github.com/devj1003/mygoapp/pkg/render"
@@ -13,10 +15,25 @@ import (
 // Port number function
 const portNumber = ":8081"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // Main func
 func main() {
 
-	var app config.AppConfig
+	// var app config.AppConfig
+
+	// change this to true when in production
+	app.InProduction = false
+
+	// Session
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
@@ -32,9 +49,17 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	http.HandleFunc("/", handlers.Repo.HomePage)
-	http.HandleFunc("/about", handlers.Repo.AboutPage)
+	// http.HandleFunc("/", handlers.Repo.HomePage)
+	// http.HandleFunc("/about", handlers.Repo.AboutPage)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-	http.ListenAndServe(portNumber, nil)
+	// http.ListenAndServe(portNumber, nil)
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -30,6 +30,10 @@ func NewHandlers(r *Repository) {
 
 // HomePage func for home page handler
 func (m *Repository) HomePage(w http.ResponseWriter, r *http.Request) {
+
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
 }
 
@@ -39,6 +43,9 @@ func (m *Repository) AboutPage(w http.ResponseWriter, r *http.Request) {
 	// perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello, again!"
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 
 	// send the data to the template
 	render.RenderTemplate(w, "about.page.html", &models.TemplateData{
